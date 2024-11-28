35a86db6049f4aac4282da5e2b1bddf59f8090b8
Pillai <apillai@dovercorp.com>
2024-11-28 20:44:46 +0530

    Modified file upload logic and added loading animation

--- Frontend/client/src/Pages/Home.tsx
+++ Frontend/client/src/Pages/Home.tsx
@@ -1,29 +1,39 @@
 import react from "react";
-import FileButton from "../components/FileButton"
-import {Box, Typography} from "@mui/material"
-import {Navbar} from "../components/Navbar"
+import { useState } from 'react';
+import { FileButton } from "../components/FileButton"
+import { Box, Typography } from "@mui/material"
+import { Navbar } from "../components/Navbar"
+import { Loading } from "../components/LoadingAnimations";
 export const Home = () => {
-    return (
-        <>
-        <Navbar />
-        <Box 
-            sx={{ 
-              display: 'flex', 
-              justifyContent: 'center', 
-              alignItems: 'center', 
-              flexDirection: 'column', 
-              marginTop: 2 // Space from Navbar
-            }}
-          >
-            <Typography variant="h6" gutterBottom>
-              Attach Files:
-            </Typography>
-            <Box sx={{ display: 'flex', gap: 2 }}>
-                <FileButton display="Attach File 1" />
-                <FileButton display="Attach File 2" />
-            </Box>
-          </Box>
+  const [showLoading, setShowLoading] = useState(false);
 
-        </>
-    )
+  const handleClick = () => {
+    setShowLoading(true);
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          marginTop: 2 // Space from Navbar
+        }}
+      >
+        <Typography variant="h6" gutterBottom>
+          Attach Files:
+        </Typography>
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <FileButton display="Upload File 1" />
+          <FileButton display="Upload File 2" />
+        </Box>
+        <button onClick={handleClick}>Submit</button>
+        {showLoading && <Loading></Loading>}
+      </Box>
+
+    </>
+  )
 } 
--- Frontend/client/src/components/FileButton.tsx
+++ Frontend/client/src/components/FileButton.tsx
@@ -1,12 +1,14 @@
-import { useRef, useEffect } from 'react';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
+import axios from 'axios';
 
 interface ButtonProp {
     display: string;
 }
 
-function FileButton({display}: ButtonProp) {
+export function FileButton({display}: ButtonProp) {
+
     //const inputRef = useRef(null)
     //useEffect(() => {
     //    function fileUploaded(this: HTMLInputElement) {
@@ -47,10 +49,29 @@ function FileButton({display}: ButtonProp) {
     //    });
     //};
 
-    const fileReader = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const [fileUploadStatus, setFileUploadStatus] = useState("");
+    
+    async function fileReader(e: React.ChangeEvent<HTMLInputElement>) {
         if (e == null || e.target == null || e.target.files == null)
             return;
         var file = e.target.files[0];
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("filename", file.name);
+        console.log(formData.getAll)
+        console.log(file.name)
+        try {
+            await axios.post("http://localhost:9292" + "/upload/rawdata", formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+                timeout: 2000
+            });
+            setFileUploadStatus(file.name);
+        }
+        catch(err) {
+            setFileUploadStatus("failed");
+        }
         const fr = new FileReader();
         fr.onload = (e) => {
             if (e == null || e.target == null)// || e.target.files == null)
@@ -74,8 +95,7 @@ function FileButton({display}: ButtonProp) {
                 {/* <input ref={inputRef} accept=".csv" type="file" hidden style={{ display: display }} /> */}
                 <input onChange={fileReader} accept=".csv" type="file" hidden style={{ display: display }} />
             </Button>
+            <p>{fileUploadStatus}</p>
         </div>
     )
 }
-
-export default FileButton;
